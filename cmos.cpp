9ea964226b1eb69d649eff7025e39217b836fbcd
Kroenigk <147001744+Kroenigk@users.noreply.github.com>
2026-02-26 01:48:57 +0000

    Added comments of to do

--- cmos.cpp
+++ cmos.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+    /// Open file
+    ///read in file info and remove white space so it is one line of characters
+    ///Choose k value
+    /// make all of the overlapping k-mers
+    /// Hash the k-mers
+    /// store sequence of hashed
+    /// choose window size
+    /// choose minimum hash value from each window
+
+    /// calculate similarity score
+
     cout << "CMOS Project" << endl;
     return 0;
 }
